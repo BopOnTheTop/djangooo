b12ef588fe751999167981a465d51a54b3725085
BopOnTheTop <bonduell@cyberdude.com>
2015-09-06 23:09:35 +0300

    created a main.css and started the template path of a jedi

--- tudulist/views.py
+++ tudulist/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.http import HttpResponse
 # Create your views here.
+
+
+def todo(request):
+    return render(request, 'todo.html', {})
+
+
+def todo_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+
