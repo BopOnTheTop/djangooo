858629a99f11cff8fd783dd7b498be8a24c31dd0
BopOnTheTop <bonduell@cyberdude.com>
2015-09-09 09:29:21 +0300

    Added a few fields. Like ID and progress

--- tudulist/models.py
+++ tudulist/models.py
@@ -4,10 +4,12 @@ from django.db import models
 # Create your models here.
 class task(models.Model):
     """Task Model"""
+    id = models.IntegerField(primary_key=True,unique=True,db_index=True)
     quest = models.CharField(max_length=256,blank=False,verbose_name=u"Квест")
-    given = models.DateField(blank=False,verbose_name=u"Видали",null=True)
+    given = models.DateField(blank=False,verbose_name=u"Видано",null=True)
     deadline= models.DateField(blank=False,verbose_name=u"Дедлайн",null=True)
     notes = models.TextField(blank=True,verbose_name=u"Додаткові нотатки")
     done = models.BooleanField(blank=True,verbose_name=u"Здано")
+    progress = models.CharField(blank=True,max_length=256,verbose_name=u"Зроблено",null=True)
     whois = models.CharField(max_length=256,blank=True,verbose_name=u"Квестодавець")
     priority = models.IntegerField(verbose_name="Пріоритет",null=True)
